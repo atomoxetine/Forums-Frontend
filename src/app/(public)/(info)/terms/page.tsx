318c4767c6bf31fcac5f6a7885b65251a5ce1f55
Emily <emily@bunni.me>
2024-01-23 12:54:04 -0500

    Update terms page with new content

--- src/app/(public)/(info)/terms/page.tsx
+++ src/app/(public)/(info)/terms/page.tsx
@@ -2,19 +2,83 @@ export default function Terms() {
   return (
     <div className="flex flex-col gap-y-2">
       <h1 className="text-rainbow text-4xl md:text-5xl lg:text-6xl font-medium font-header lg:max-w-[72rem] mx-auto">
-        Privacy Policy
+        Terms of Service
       </h1>
       <p className="text-warning font-header font-medium mx-auto">
         Effective Date:  January 11th, 2024 | Last Updated:  January 11th, 2024
       </p>
       <div className="flex flex-col md:gap-y-4 mt-10">
         <p>
-          Welcome to PinkCloud Studios ("Company," "Collective," "we," "us," or "our" ). This Privacy Policy explains how we collect, use, disclose, and protect your personal information when you use our website, products, and services (collectively, the "Services"). By using our Services, you agree to the terms of this Privacy Policy.
+        These Terms of Service ("Terms") govern your use of the services provided by MCCade Games (the "Server", the "Company") through our services. By accessing and/or using our software
+         and/or website and the services provided through it (collectively, the "server"), you agree to comply with and be bound by the following terms and conditions of use ("Terms of Service").
+           If you do not or have failed to comply with any of our terms and conditions you are prohibited from using or accessing this site and/or the entirety of our services.
         </p>
-        <h4 className="font-bold text-secondary">1. Acceptance of Terms</h4>
+        <h4 className="font-bold text-secondary">Trademarks and Copyright</h4>
         <p>
-          By using our Services, you acknowledge that you have read, understood, and agree to be bound by these Terms of Service, as well as our Privacy Policy.
+        Minecraft content and materials are trademarks and copyrights of Mojang AB or its licensors. All rights reserved. This site is made by PinkCloud Studios and is licensed
+         to MCCade Games and is not affiliated with Mojang AB. Emoji icons provided free by EmojiOne
         </p>
+        <h4 className="font-bold text-secondary">Registering</h4>
+        <p>By registering to any MCCade services you agree to these terms and conditions:</p>
+        <p>MCCade reserves the right to deny or reject any username.</p>
+        <h4 className="font-bold text-secondary">Modification of the Terms and Conditions</h4>
+        <p>
+        We reserve the right to change this terms and conditions from time to time without notice. You acknowledge and agree that it is your responsibility to review this User Agreement 
+        periodically to familiarize yourself with any modifications. Your continued use of this site after such modifications will constitute acknowledgment and agreement of the modified
+        terms and conditions.
+        </p>
+        <h4 className="font-bold text-secondary">Web Site/Server Terms and Conditions of Use</h4>
+        <ol>
+          <li>
+            <h5 className="font-bold text-secondary">Terms</h5>
+            <p>
+            By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible
+            for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in
+            this web site are protected by applicable copyright and trade mark law.
+            </p>
+          </li>
+          <li>
+            <h5 className="font-bold text-secondary">Disclaimer</h5>
+            <p>
+            The materials on MCCade's web site are provided "as is". MCCade Games makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, 
+            including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other 
+            violation of rights. Furthermore, Mineplex does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials 
+            on its Internet web site or otherwise relating to such materials or on any sites linked to this site.  
+            </p>
+          </li>
+          <li>
+            <h5 className="font-bold text-secondary">Limitations</h5>
+            <p>
+            In no event shall MCCade or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) 
+            arising out of the use or inability to use the materials on MCCade's Internet site, even if MCCade or a MCCade authorized representative has been notified orally 
+            or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential 
+            or incidental damages, these limitations may not apply to you. 
+            </p>
+          </li>
+          <li>
+            <h5 className="font-bold text-secondary">Revisions and Errata</h5>
+            <p>
+            The materials appearing on MCCade's web site could include technical, typographical, or photographic errors. MCCade does not warrant that any of the materials on its web 
+            site are accurate, complete, or current. MCCade may make changes to the materials contained on its web site at any time without notice. MCCade does not, however, make any 
+            commitment to update the materials.  
+            </p>
+          </li>
+          <li>
+            <h5 className="font-bold text-secondary">Links</h5>
+            <p>
+            MCCade has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does 
+            not imply endorsement by MCCade of the site. Use of any such linked web site is at the user's own risk.  
+            </p>
+          </li>
+          <li>
+            <h5 className="font-bold text-secondary">Governing Law</h5>
+            <p>
+            Any claim relating to MCCade's web site shall be governed by the laws of Australia without regard to its conflict of law provisions.
+            </p>
+          </li>
+        </ol>
+
+ 
       </div>
     </div>
   );
