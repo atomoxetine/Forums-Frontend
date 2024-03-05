74211ba95db9755ec288ed786c2cf52bb2e7e255
atomoxetine <emilybertollo@outlook.com>
2024-03-05 18:03:04 -0300

    feat: added "staff -> user page" navigation

--- src/app/(main)/(withHeaderContent)/staff/page.tsx
+++ src/app/(main)/(withHeaderContent)/staff/page.tsx
@@ -1,6 +1,8 @@
+import HashLink from "@/components/HashLink";
 import HeaderContext from "@/components/HeaderContext";
 import { ServerMCHead } from "@/components/Minecraft/Server";
 import { getRankColor, getRankName, getStaffUsers, getUsernameFromUuid } from "@/services/forum/account/AccountService";
+import Link from "next/link";
 import React from "react";
 
 export default async function Staff() {
@@ -36,13 +38,17 @@ export default async function Staff() {
     <div className="window flex flex-row flex-wrap h-min w-screen max-w-full bg-base-300 rounded-xl p-2 gap-2">
       {listPerRank.map((d, i) => (
         <div key={i} className="flex-[1_0_32%] flex flex-col justify-center items-center rounded-lg overflow-hidden bg-base-100 py-4">
-          <div className="w-[45px] h-[49px] relative">
-            <ServerMCHead shadowColor={d.color} className="scale-[.675] absolute left-[-11px] top-[-12px]" username={d.username} />
-          </div>
-          <span className="text-center inline-flex flex-col">
-            <h5 className="font-bold">{d.username}</h5>
-            <small style={{color: d.color}} className="smaller font-bold uppercase tracking-wider">{d.rank}</small>
-          </span>
+          <Link href={`/u/${d.username}`}>
+            <div className="w-[45px] h-[49px] relative">
+              <ServerMCHead shadowColor={d.color} className="scale-[.675] absolute left-[-11px] top-[-12px]" username={d.username} />
+            </div>
+          </Link>
+          <Link href={`/u/${d.username}`}>
+            <span className="text-center inline-flex flex-col">
+              <h5 className="font-bold">{d.username}</h5>
+              <small style={{color: d.color}} className="smaller font-bold uppercase tracking-wider">{d.rank}</small>
+            </span>
+          </Link>
         </div>
       ))}
     </div>
